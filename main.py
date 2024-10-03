dfb4cbfbcdee88e7f2c83ff5d390e54dd016fc81
samuel espinosa arias <76191247+hola0511@users.noreply.github.com>
2024-10-02 19:10:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ templates = Jinja2Templates(directory=".")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "message": "¡Hola, una página WEB que interactúa con backend usando FastAPI con HTML!", "X": [1, 2, 3, 4, 5, 6,7, 8]})
+    return templates.TemplateResponse("index.html", {"request": request, "message": "Hola profe", "X": [1, 2, 3, 4, 5, 6, 7, 8]})
