8837d0bd2483ec8b824b3fe98c0e1900e8b7ffe2
samuel espinosa arias <76191247+hola0511@users.noreply.github.com>
2024-10-02 19:36:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ templates = Jinja2Templates(directory=".")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    mensaje = "Hola profe"
+    mensaje = "ARRIBA"
     lista =  [1, 2, 3, 4, 5, 6, 7, 8]
-    return templates.TemplateResponse("index.html", {"request": request, "message":mensaje, "X":lista})
+    ci = "ESPAÑA"
+    return templates.TemplateResponse("index.html", {"request": request, "message":mensaje, "X":lista}, "ci":ci)
